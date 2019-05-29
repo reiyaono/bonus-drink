82242d3d17baf4ed83e26338f872d79b923a547a
reiya <reiya.ono@linkbal.co.jp>
2019-05-29 18:17:42 +0900

    add method & test, fix environment

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
+  GET_BOTTLE_COUNT = 3 
   def self.total_count_for(amount)
-    0
+    amount = remaining = amount.to_i
+    while remaining.div(GET_BOTTLE_COUNT).positive?
+      get = remaining.div(GET_BOTTLE_COUNT)
+      remaining = get + remaining % GET_BOTTLE_COUNT
+      amount += get
+    end
+    amount
   end
 end
+
+p ARGV[0]
+p BonusDrink.total_count_for(ARGV[0])
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  context 'when given ARVG' do
+    it 'sould be expected num' do
+      expect(BonusDrink.total_count_for(0)).to eq 0
+      expect(BonusDrink.total_count_for(1)).to eq 1
+      expect(BonusDrink.total_count_for(3)).to eq 4
+      expect(BonusDrink.total_count_for(11)).to eq 16
+      expect(BonusDrink.total_count_for(100)).to eq 149
+    end
+  end
 end
